cc2da19e73fb4d8010428a2332efcfcedc42a938
TheFlyer1983 <theflyer1983@gmail.com>
2019-02-16 19:18:47 +0000

    The first part of the interpolated string C# lesson

---
+++ interpolated/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace interpolated
+{
+    public class Vegetable
+    {
+      public Vegetable(string name) => Name = name;
+
+      public string Name { get; }
+
+      public override string ToString() => Name;
+    }
+
+    public class Program
+    {
+      public enum Unit { item, kilogram, gram, dozen };
+
+      public static void Main()
+      {
+        var item = new Vegetable("eggplant");
+        var date = DateTime.Now;
+        var price = 1.99m;
+        var unit = Unit.item;
+        Console.WriteLine($"On {date:d}, the price of {item} was {price:C2} per {unit}.");
+      }
+    }
+    
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var name = "Paul";
+    //        Console.WriteLine($"Hello, {name}. It's a pleasure to meet you");
+    //    }
+    //}
+}
